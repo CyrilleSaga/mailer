a2752cffd2f7ef6b8d7bb3c9c6dd94bae9a31ccb
Cyrille TOUMI <cyrilletoumi8@gmail.com>
2024-05-02 18:25:33 +0100

    Add CORS in headers

--- index.js
+++ index.js
@@ -7,6 +7,19 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 
 app.get("/", (req, res) => {
   res.send({ hello: "World" });
