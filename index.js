421b1e60c768359822598d2e56ea407b2430db8e
Cyrille TOUMI <cyrilletoumi8@gmail.com>
2024-05-02 11:57:05 +0100

    Change server PORT

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.post("/api/sendmail", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3665;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
